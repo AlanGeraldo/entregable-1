70e6ca9f04a07d2eda566d471bda3467f8705fcd
Alan Geraldo <fcoalan_97@outlook.com>
2023-07-31 21:23:59 -0700

    Agregando favicon y titulo

--- src/components/BoxInfo.jsx
+++ src/components/BoxInfo.jsx
@@ -7,29 +7,25 @@ const BoxInfo = ({handleClick, dataB}) => {
         </div>
 
         <section>
+
             <div className='box-btn'>
-            <button className='btn' onClick={handleClick}> <i class='bx bx-revision'></i>   </button>
+                <button className='btn' onClick={handleClick}> <i class='bx bx-revision'></i>   </button>
             </div>
             <article className='box-phrase'>
-
                 <div className='bg-phrase'></div>
-
                 <p>
                         <b>{dataB.phrase}</b> 
                 </p>
-                
             </article>
             
         </section>
-
             <footer className='box-footer'>
                 <div className='planet-footer'></div>
-                <h4> 
-                    Fuente: {dataB.author} 
-                </h4>
+                    <h4> 
+                        Fuente: {dataB.author} 
+                    </h4>
             </footer>
-        
-    </section>
+        </section>
     )
 }
 export default BoxInfo
